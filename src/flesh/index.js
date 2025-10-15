bff62cdfb14db278c59c610e9a00f690c0cd6dc3
Ignacio Garcia Villanueva <iggy.dev@pm.me>
2025-10-15 23:59:45 +1000

    added 404 and commented other useful code

--- src/flesh/index.js
+++ src/flesh/index.js
@@ -15,22 +15,28 @@ const server = http.createServer((req, res) => {
         res.end(html);
         return;
     }
-    if (req.url !== '/') {
-        const [root,type,component] = req.url.split('/');
-        const test = import(fileURLToPath(new URL(`../components/${type}s/${component}/${component}.show.js`, import.meta.url))).then((module) => {
-        const show = module.show();
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end(show);
-        }).catch((error) => {
-        res.writeHead(404, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify({ error: 'Component not found', details: error.message }));
-        });
-    } else {
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify({
-            data: 'Flesh Server is running. Append /type/componentName to the URL to run a specific component test (e.g., atom/text, /box, /video, etc.).',
-        }));
-    }
+    // Handle 404
+    res.writeHead(404, { 'Content-Type': 'text/html' });
+    res.end(renderPage(
+        '<h1>404 - Page Not Found</h1>',
+        { title: '404' }
+    ));
+    // if (req.url !== '/') {
+    //     const [root,type,component] = req.url.split('/');
+    //     const test = import(fileURLToPath(new URL(`../components/${type}s/${component}/${component}.show.js`, import.meta.url))).then((module) => {
+    //     const show = module.show();
+    //     res.writeHead(200, { 'Content-Type': 'text/html' });
+    //     res.end(show);
+    //     }).catch((error) => {
+    //     res.writeHead(404, { 'Content-Type': 'application/json' });
+    //     res.end(JSON.stringify({ error: 'Component not found', details: error.message }));
+    //     });
+    // } else {
+    //     res.writeHead(200, { 'Content-Type': 'application/json' });
+    //     res.end(JSON.stringify({
+    //         data: 'Flesh Server is running. Append /type/componentName to the URL to run a specific component test (e.g., atom/text, /box, /video, etc.).',
+    //     }));
+    // }
 })
 
 server.listen(3000, ()=>{
